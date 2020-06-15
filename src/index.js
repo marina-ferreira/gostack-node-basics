cd5c3830fc727f56afbf9d555d3d71e70aae93e6
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-15 20:34:38 -0300

    Express json

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/projects', (request, response) => {
-  return response.send({ message: 'Hello World' })
- })
+  return response.json([
+    'Project 1',
+    'Project 2'
+  ])
+})
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+    'Project 3'
+  ])
+})
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Project 4',
+    'Project 2',
+    'Project 3'
+  ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Project 2',
+    'Project 3'
+  ])
+})
 
 app.listen(4000, () => console.log('🌹 Server started on port 4000'))
