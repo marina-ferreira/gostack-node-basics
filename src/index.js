568a9bc005c897625da8365316f86266b3c67b35
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-15 21:02:40 -0300

    Projects index, show, post, put and delete endpoints

--- src/index.js
+++ src/index.js
@@ -1,37 +1,61 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
 app.use(express.json())
 
+const projects = []
+
 app.get('/projects', (request, response) => {
-  return response.json([
-    'Project 1',
-    'Project 2'
-  ])
+  return response.json(projects)
+})
+
+app.get('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const project = projects.find(project => project.id === id)
+
+  if (!project) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  return response.json(project)
 })
 
 app.post('/projects', (request, response) => {
-  return response.json([
-    'Project 1',
-    'Project 2',
-    'Project 3'
-  ])
+  const { title, owner } = request.body
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project)
+  return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'Project 4',
-    'Project 2',
-    'Project 3'
-  ])
+  const { id } = request.params
+  const { title, owner } = request.body
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  const project = { id, title, owner }
+  projects[projectIndex] = project
+
+  return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Project 2',
-    'Project 3'
-  ])
+  const { id } = request.params
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(4000, () => console.log('🌹 Server started on port 4000'))
