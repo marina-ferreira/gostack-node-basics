d363348cae58d95842af550b1eee9121f3cb37a1
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-15 21:32:51 -0300

    Log Middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ app.use(express.json())
 
 const projects = []
 
+function logRequests(request, response, next) {
+  const { method, url } = request
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+  console.log(logLabel)
+
+  return next()
+}
+
+app.use(logRequests)
+
 app.get('/projects', (request, response) => {
   const { title } = request.query
   const results = title
